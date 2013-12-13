1a5b9f5b91d8fc49d8daef58e889937a89869ee3
omaximchuk <omaximchuk@guardian.com>
2013-12-13 13:52:02 +0400

    discrete time algorithm corrections

--- src/SimpleAlgorithm.java
+++ src/SimpleAlgorithm.java
@@ -5,18 +5,18 @@ import static java.lang.Math.max;
 public class SimpleAlgorithm {
 
     Double s0 = 1.0;
-    Double fixedCosts = 0.01;
+    Double fixedCosts = 0.0;
     Double p = 0.9;
     Double q = 1 - p;
     Double up = 1.1;
     Double down = 0.5;
-    Double propCosts = 0.0;
-    Integer N = 100;
-    Double[][] v1 = new Double[N][N + 1];
-    Double[][] v0 = new Double[N][N + 1];
-    Double[][] s = new Double[N][N + 1];
-    Integer[][] u0 = new Integer[N][N + 1];
-    Integer[][] u1 = new Integer[N][N + 1];
+    Double propCosts = 0.01;
+    Integer N = 10;
+    Double[][] v1 = new Double[N + 1][N];
+    Double[][] v0 = new Double[N + 1][N];
+    Double[][] s = new Double[N + 1][N];
+    Integer[][] u0 = new Integer[N + 1][N];
+    Integer[][] u1 = new Integer[N + 1][N];
 
     public SimpleAlgorithm() {
         super();
@@ -26,23 +26,23 @@ public class SimpleAlgorithm {
 
     private void initArrays() {
         s[0][0] = s0;
-        for (int i = 1; i < s.length; i++)
-            for (int j = 0; j <= i; j++) {
-                s[i][j] = Math.pow(up, i - j) * Math.pow(down, j);
+        for (int j = 1; j < N; j++)
+            for (int i = 0; i <= j; i++) {
+                s[i][j] = Math.pow(up, j - i) * Math.pow(down, i);
                 u0[i][j] = 0;
                 u1[i][j] = 0;
             }
         for (int i = 0; i < N; i++) {
-            v0[N - 1][i] = 0.0;
-            v1[N - 1][i] = 0.0;//(1 - propCosts) * s[N-1][i] - fixedCosts;
+            v0[i][N - 1] = 0.0;
+            v1[i][N - 1] = 0.0;//(1 - propCosts) * s[N-1][i] - fixedCosts;
         }
     }
 
     private void processArrays() {
-        for (int i = N - 2; i >= 0; i--)
-            for (int j = 0; j <= i; j++) {
-                Double m1 = f(v0[i + 1][j], v0[i + 1][j + 1]);
-                Double n1 = f(v1[i + 1][j], v1[i + 1][j + 1]);
+        for (int j = N - 2; j >= 0; j--)
+            for (int i = 0; i <= j; i++) {
+                Double m1 = f(v0[i][j + 1], v0[i + 1][j + 1]);
+                Double n1 = f(v1[i][j + 1], v1[i + 1][j + 1]);
                 Double m2 = (-1 - propCosts) * s[i][j] - fixedCosts + n1;
                 Double n2 = (1 - propCosts) * s[i][j] - fixedCosts + m1;
                 v0[i][j] = Math.max(m1, m2);
@@ -53,9 +53,9 @@ public class SimpleAlgorithm {
     }
 
     public void printArray(Number[][] array) {
-        for (int i = array.length - 2; i >= 0; i--) {
-            System.out.print("i = " + (i + 1) + ":" + new String(new char[array.length - i]).replace("\0", "  "));
-            for (int j = 0; j <= i; j++)
+        for (int j = array.length - 2; j >= 0; j--) {
+            System.out.print("time step = " + (j + 1) + ":" + new String(new char[array.length - j]).replace("\0", "  "));
+            for (int i = 0; i <= j; i++)
                 if (array[i][j].doubleValue() >= 0)
                     System.out.print((j != i) ? " " + array[i][j] + " | " : " " + array[i][j]);
                 else
